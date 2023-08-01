75ead60f1f9fb960b294c64d10fd2ad8df1c1ddc
QusDev <qusdeveloper@gmail.com>
2023-08-01 19:28:58 +0300

    add validation for sign up

--- WebShop/Application/CQRS/Identity/Users/Commands/CreateUser/CreateUserCommandValidation.cs
+++ WebShop/Application/CQRS/Identity/Users/Commands/CreateUser/CreateUserCommandValidation.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebShop.Application.CQRS.Catalog.Categories.Commands.CreateCategory;
 
 namespace WebShop.Application.CQRS.Identity.Users.Commands.CreateUser
 {
-    public class CreateUserCommandValidation
+    public class CreateUserCommandValidation : AbstractValidator<CreateUserCommand>
     {
+        public CreateUserCommandValidation()
+        {
+            RuleFor(c => c.Password)
+                .NotEmpty()
+                .MinimumLength(8)
+                .MaximumLength(64);
+               
+            RuleFor(c => c.FirstName)
+                .NotEmpty()
+                .MinimumLength(2)
+                .MaximumLength(128);
+
+            RuleFor(c => c.LastName)
+                .NotEmpty()
+                .MinimumLength(2)
+                .MaximumLength(128);
+
+            RuleFor(c => c.Email)
+                .NotEmpty()
+                .Matches("[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?");
+        }
     }
 }
