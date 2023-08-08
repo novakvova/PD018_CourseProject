5ab743f901a0953303e1c93e02d507cb778eca07
novakvova <novakvova@gmail>
2023-08-08 15:31:54 +0300

    host app is good

--- WebShop/WebShop.WebAPI/Program.cs
+++ WebShop/WebShop.WebAPI/Program.cs
@@ -49,10 +49,10 @@ builder.Services.AddCors(options => {
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if ( app.Environment.IsDevelopment() ) {
+//if ( app.Environment.IsDevelopment() ) {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
